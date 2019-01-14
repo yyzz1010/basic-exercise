f9ed6d9e60631ea5c113693502d2e6c1a7f34026
yyzz1010 <45536868+yyzz1010@users.noreply.github.com>
2019-01-14 20:34:12 +0800

    Update 1. guess_number_game.py

--- 1. guess_number_game.py
+++ 1. guess_number_game.py
@@ -1,7 +1,7 @@
 #請做出一支能猜數字的程式：每次讓使用者猜一個整數，若猜對就輸出Bingo；使用者最多可以猜3次。(提示: Bingo後可以使用break來離開迴圈)
 
 a = int(input("Guess a number? (1-10) "))
-times = 0
+times = 1
 
 import random
 c_choice = random.randint(1,10)
@@ -11,9 +11,8 @@ for i in range(3):
     print("Bingo")
     break
 
-  elif times == 2:
+  elif times == 3:
     print("Game Over!")
-    break
   
   elif a > c_choice:
     a = int(input("The answer is smaller. Try again~ "))
